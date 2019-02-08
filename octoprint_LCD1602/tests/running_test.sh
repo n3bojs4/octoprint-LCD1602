5f4e2c933955201963877d53b67c1a69c8e66610
nebo <n3bojs4@gmail.com>
2019-02-08 01:10:40 +0100

    Update octoprint_LCD1602/tests/running_test.sh

--- octoprint_LCD1602/tests/running_test.sh
+++ octoprint_LCD1602/tests/running_test.sh
@@ -4,13 +4,25 @@ source /opt/octoprint/venv/bin/activate
 python setup.py install || echo "Error: Cannot install LCD1602 Plugin"
 
 echo "Starting octoprint server"
-/opt/octoprint/venv/bin/octoprint serve >/tmp/logs & || echo "Error: Cannot start the server !"
+/opt/octoprint/venv/bin/octoprint serve >/tmp/logs || echo "Error: Cannot start the server !" &
+
+echo "Checking octoprint is running"
+sleep 3
+RUNNING=$(pgrep -c octoprint)
+
+
+if [ $RUNNING -eq 0 ]
+then
+  echo 'Octoprint failed to start' && exit 1
+else
+  echo 'Octoprint is started'
+fi
 
 echo "Looking for errors on logs"
 sleep 10
 ERRORS=$(grep -i -c error /tmp/logs )
 
-if ( $ERRORS -gt 0 )
+if [ $ERRORS -gt 0 ]
 then
   exit 1
 else
